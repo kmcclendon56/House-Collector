992ecba5472a0bcd5d7c94e2d9f474e1649bd679
kmcclendon <kmcclendon56@gmail.com>
2022-08-29 13:48:20 -0500

    added a house model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class House(models.Model):
+    address = models.CharField(max_length=100)
+    rooms_baths = models.CharField(max_length=10)
+    sqft = models.IntegerField()
+    gas_stove = models.BooleanField()
+    notes = models.CharField(max_length=300)
+    allows_dogs = models.BooleanField()
