3565bb0d6aa254b6e7efc1e28c67f77dd92f42d7
kmcclendon <kmcclendon56@gmail.com>
2022-08-29 17:22:45 -0500

    Added a create house class

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('houses/', views.houses_index, name='index'),
     path('houses/<int:house_id>/', views.houses_detail, name='detail'),
+    path('houses/create/', views.HouseCreate.as_view(), name='houses_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import House
+from django.views.generic.edit import CreateView
 
 
 
@@ -13,9 +14,13 @@ def about(request):
     return render(request, 'about.html')
 
 def houses_index(request):
-    house = House.objects.all()
+    houses = House.objects.all()
     return render(request, 'houses/index.html', { 'houses': houses })
 
 def houses_detail(request, house_id):
     house = House.objects.get(id=house_id)
     return render(request, 'houses/detail.html', {'house': house})
+
+class HouseCreate(CreateView):
+    model = House
+    fields = '__all__'
