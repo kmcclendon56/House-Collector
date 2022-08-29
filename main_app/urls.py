1dfcf0fca0b3d5a1cc57afbeaeff24d65d6b5113
kmcclendon <kmcclendon56@gmail.com>
2022-08-29 17:07:48 -0500

    Fixing views and url. Changed the about.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('houses/', views.houses_index, name='index'),
-    path('houses/<int:house_id>', views.houses_detail, name='detail')
+    path('houses/<int:house_id>/', views.houses_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -7,14 +7,15 @@ from .models import House
 
 
 def home(request):
-  return HttpResponse('<h1>Welcome to the House Collector</h1>')
+    return HttpResponse('<h1>Welcome to the House Collector</h1>')
 
 def about(request):
     return render(request, 'about.html')
 
 def houses_index(request):
-  return render(request, 'houses/index.html', { 'houses': houses })
+    house = House.objects.all()
+    return render(request, 'houses/index.html', { 'houses': houses })
 
-def house_details(request, house_id):
+def houses_detail(request, house_id):
     house = House.objects.get(id=house_id)
-    return render(request, 'house/detail.html', {'house': house})
+    return render(request, 'houses/detail.html', {'house': house})
