ebcaf67dab8bb0ef5aa92d610cd3af9cc850f18b
kmcclendon <kmcclendon56@gmail.com>
2022-08-29 22:53:23 -0500

    Fixed button and kinda fixed check box

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import House
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
-
+from .models import House
 
 
 
